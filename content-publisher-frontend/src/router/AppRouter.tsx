1c9430d4b2b18eabd5934be10d61c1de67bd84af
NeerajRatho <neerajrathor970@gmail.com>
2025-11-26 14:15:00 +0530

    Added ProtectedRoute

--- content-publisher-frontend/src/router/AppRouter.tsx
+++ content-publisher-frontend/src/router/AppRouter.tsx
@@ -1,19 +1,38 @@
-import { RouterProvider, createBrowserRouter, Navigate } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "../screens/Login";
 import Signup from "../screens/Signup";
 import PublicationsList from "../screens/PublicationsList";
 import AddPublication from "../screens/AddPublication";
 import EditPublication from "../screens/EditPublication";
+import ProtectedRoute from "./ProtectedRoute";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: localStorage.getItem("token") ? <PublicationsList /> : <Navigate to="/signup" />,
+    element: (
+      <ProtectedRoute>
+        <PublicationsList />
+      </ProtectedRoute>
+    ),
   },
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <Signup /> },
-  { path: "/add", element: <AddPublication /> },
-  { path: "/edit/:id", element: <EditPublication /> },
+  {
+    path: "/add",
+    element: (
+      <ProtectedRoute>
+        <AddPublication />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/edit/:id",
+    element: (
+      <ProtectedRoute>
+        <EditPublication />
+      </ProtectedRoute>
+    ),
+  },
 ]);
 
 export default function AppRouter() {
